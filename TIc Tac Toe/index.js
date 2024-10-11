5bb28f1409d6be6caba001e03094a012100a32a9
Durgaprasad-Developer <BDurga.24BCS10012@sst.scaler.com>
2024-10-11 13:38:24 +0530

    Update index.js

--- TIc Tac Toe/index.js
+++ TIc Tac Toe/index.js
@@ -1,116 +1,77 @@
-const boxes = document.querySelectorAll(".box");
-const gameInfo = document.querySelector(".game-info");
-const newGameBtn = document.querySelector(".btn");
+const boxes = document.querySelectorAll('.box');
+const gameInfo = document.querySelector('.game-info');
+const newGameBtn = document.querySelector('.btn');
 
+let currentPlayer = 'X';
+let gameActive = true;
+let gameState = ['', '', '', '', '', '', '', '', '', ''];
 
-let currentPlayer;
-let gameGrid;
-
-const winningPositions = [
-    [0,1,2],
-    [3,4,5],
-    [6,7,8],
-    [0,3,6],
-    [1,4,7],
-    [2,5,8],
-    [0,4,8],
-    [2,4,6]
+const winningConditions = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
 ];
 
-//let's create a function to initialise the game
-function initGame() {
-    currentPlayer = "X";
-    gameGrid = ["","","","","","","","",""];
-    //UI pr empty bhi karna padega boxes ko
-    boxes.forEach((box, index) => {
-        box.innerText = "";
-        boxes[index].style.pointerEvents = "all";
-        //one more thing is missing, initialise box with css properties again
-        box.classList = `box box${index+1}`;
-    });
-    newGameBtn.classList.remove("active");
-    gameInfo.innerText = `Current Player - ${currentPlayer}`;
-}
+const handleBoxClick = (index) => {
+  if (gameState[index] !== '' || !gameActive) {
+    return;
+  }
 
-initGame();
+  gameState[index] = currentPlayer;
+  boxes[index].textContent = currentPlayer;
 
-function swapTurn() {
-    if(currentPlayer === "X") {
-        currentPlayer = "O";
+  checkResult();
+};
+
+const checkResult = () => {
+  let roundWon = false;
+
+  for (let i = 0; i < winningConditions.length; i++) {
+    const [a, b, c] = winningConditions[i];
+    if (gameState[a] === '' || gameState[b] === '' || gameState[c] === '') {
+      continue;
     }
-    else {
-        currentPlayer = "X";
+    if (gameState[a] === gameState[b] && gameState[a] === gameState[c]) {
+      roundWon = true;
+      break;
     }
-    //UI Update
-    gameInfo.innerText = `Current Player - ${currentPlayer}`;
-}
+  }
 
-function checkGameOver() {
-    let answer = "";
+  if (roundWon) {
+    gameInfo.textContent = `${currentPlayer} has won!`;
+    gameActive = false;
+    return;
+  }
 
-    winningPositions.forEach((position) => {
-        //all 3 boxes should be non-empty and exactly same in value
-        if( (gameGrid[position[0]] !== "" || gameGrid[position[1]] !== "" || gameGrid[position[2]] !== "") 
-            && (gameGrid[position[0]] === gameGrid[position[1]] ) && (gameGrid[position[1]] === gameGrid[position[2]])) {
+  if (!gameState.includes('')) {
+    gameInfo.textContent = `It's a draw!`;
+    gameActive = false;
+    return;
+  }
 
-                //check if winner is X
-                if(gameGrid[position[0]] === "X") 
-                    answer = "X";
-                else {
-                    answer = "O";
-                } 
-                    
+  currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+  gameInfo.textContent = `It's ${currentPlayer}'s turn`;
+};
 
-                //disable pointer events
-                boxes.forEach((box) => {
-                    box.style.pointerEvents = "none";
-                })
-
-                //now we know X/O is a winner
-                boxes[position[0]].classList.add("win");
-                boxes[position[1]].classList.add("win");
-                boxes[position[2]].classList.add("win");
-            }
-    });
-
-    //it means we have a winner
-    if(answer !== "" ) {
-        gameInfo.innerText = `Winner Player - ${answer}`;
-        newGameBtn.classList.add("active");
-        return;
-    }
-
-    //We know, NO Winner Found, let's check whether there is tie
-    let fillCount = 0;
-    gameGrid.forEach((box) => {
-        if(box !== "" )
-            fillCount++;
-    });
-
-    //board is Filled, game is TIE
-    if(fillCount === 9) {
-        gameInfo.innerText = "Game Tied !";
-        newGameBtn.classList.add("active");
-    }
-
-}
-
-function handleClick(index) {
-    if(gameGrid[index] === "" ) {
-        boxes[index].innerText = currentPlayer;
-        gameGrid[index] = currentPlayer;
-        boxes[index].style.pointerEvents = "none";
-        //swap karo turn ko
-        swapTurn();
-        //check koi jeet toh nahi gya
-        checkGameOver();
-    }
-}
+const resetGame = () => {
+  gameActive = true;
+  currentPlayer = 'X';
+  gameState = ['', '', '', '', '', '', '', '', '', ''];
+  gameInfo.textContent = `It's ${currentPlayer}'s turn`;
+  boxes.forEach(box => {
+    box.textContent = '';
+  });
+};
 
 boxes.forEach((box, index) => {
-    box.addEventListener("click", () => {
-        handleClick(index);
-    })
+  box.addEventListener('click', () => handleBoxClick(index));
 });
 
-newGameBtn.addEventListener("click", initGame);
+newGameBtn.addEventListener('click', resetGame);
+
+resetGame();
